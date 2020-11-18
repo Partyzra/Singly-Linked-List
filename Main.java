3e459371af2c9a4bbfb6530382f25a76c7c6e3c4
Ryan <47995762+Partyzra@users.noreply.github.com>
2020-11-18 01:17:53 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,20 @@
+public class Main {
+
+    public static void main(String[] args) {
+        /**
+         * Definition for singly-linked list.
+         * public class ListNode {
+         *     int val;
+         *     ListNode next;
+         *     ListNode() {}
+         *     ListNode(int val) { this.val = val; }
+         *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+         * }
+         *
+        class Solution {
+            public int getDecimalValue(ListNode head) {
+
+            }
+        }
+    }
+}
